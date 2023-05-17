43bde011ab01362f19bf4e80241c7b4e4364703d
AdityaDighe <98305705+AdityaDighe@users.noreply.github.com>
2023-05-17 18:44:13 +0530

    Update graph_colouring.c

--- aoa sem4/greedy algorithm/graph_colouring.c
+++ aoa sem4/greedy algorithm/graph_colouring.c
@@ -1,98 +1,54 @@
-#include <stdbool.h>
-#include <stdio.h>
- 
-// Number of vertices in the graph
-#define V 4
- 
-void printSolution(int color[]);
- 
-// check if the colored
-// graph is safe or not
-bool isSafe(bool graph[V][V], int color[])
-{
-    // check for every edge
-    for (int i = 0; i < V; i++)
-        for (int j = i + 1; j < V; j++)
-            if (graph[i][j] && color[j] == color[i])
-                return false;
-    return true;
-}
- 
-/* This function solves the m Coloring
-   problem using recursion. It returns
-  false if the m colours cannot be assigned,
-  otherwise, return true and prints
-  assignments of colours to all vertices.
-  Please note that there may be more than
-  one solutions, this function prints one
-  of the feasible solutions.*/
-bool graphColoring(bool graph[V][V], int m, int i,
-                   int color[V])
-{
-    // if current index reached end
-    if (i == V) {
-        // if coloring is safe
-        if (isSafe(graph, color)) {
-            // Print the solution
-            printSolution(color);
-            return true;
+#include<stdio.h>
+#include<stdbool.h>
+#define NODE 6
+
+int graph[NODE][NODE] = {
+   {0, 1, 1, 1, 0, 0},
+   {1, 0, 0, 1, 1, 0},
+   {1, 0, 0, 1, 0, 1},
+   {1, 1, 1, 0, 1, 1},
+   {0, 1, 0, 1, 0, 1},
+   {0, 0, 1, 1, 1, 0}
+};
+
+void graphColoring() {
+   int color[NODE];
+   color[0] = 0;    //Assign first color for the first node
+   bool colorUsed[NODE];    //Used to check whether color is used or not
+
+   for(int i = 1; i<NODE; i++)
+      color[i] = -1;    //initialize all other vertices are unassigned
+
+   for(int i = 0; i<NODE; i++)
+      colorUsed[i] = false;    //initially any colors are not chosen
+         
+   for(int u = 1; u<NODE; u++) {    //for all other NODE - 1 vertices
+      for(int v = 0; v<NODE; v++) {
+         if(graph[u][v]){
+            if(color[v] != -1)    //when one color is assigned, make it unavailable
+               colorUsed[color[v]] = true;
+         }
+     }
+
+     int col;
+     for(col = 0; col<NODE; col++)
+        if(!colorUsed[col])    //find a color which is not assigned
+           break;
+         
+     color[u] = col;    //assign found color in the list
+         
+     for(int v = 0; v<NODE; v++) {    //for next iteration make color availability to false
+        if(graph[u][v]) {
+           if(color[v] != -1)
+              colorUsed[color[v]] = false;
         }
-        return false;
-    }
- 
-    // Assign each color from 1 to m
-    for (int j = 1; j <= m; j++) {
-        color[i] = j;
- 
-        // Recur of the rest vertices
-        if (graphColoring(graph, m, i + 1, color))
-            return true;
- 
-        color[i] = 0;
-    }
- 
-    return false;
+     }  
+  }
+   
+  for(int u = 0; u<NODE; u++)
+     printf("Color: %d, Assigned with Color: %d\n",u,color[u]);
 }
- 
-/* A utility function to print solution */
-void printSolution(int color[])
-{
-    printf("Solution Exists:"
-           " Following are the assigned colors \n");
-    for (int i = 0; i < V; i++)
-        printf(" %d ", color[i]);
-    printf("\n");
-}
- 
-// Driver code
-int main()
-{
-    /* Create following graph and
-       test whether it is 3 colorable
-      (3)---(2)
-       |   / |
-       |  /  |
-       | /   |
-      (0)---(1)
-    */
-    bool graph[V][V] = {
-        { 0, 1, 1, 1 },
-        { 1, 0, 1, 0 },
-        { 1, 1, 0, 1 },
-        { 1, 0, 1, 0 },
-    };
-    int m = 3; // Number of colors
- 
-    // Initialize all color values as 0.
-    // This initialization is needed
-    // correct functioning of isSafe()
-    int color[V];
-    for (int i = 0; i < V; i++)
-        color[i] = 0;
- 
-    // Function call
-    if (!graphColoring(graph, m, 0, color))
-        printf("Solution does not exist");
- 
-    return 0;
+
+main() {
+   graphColoring();
 }
