4476af9930b61d454c2faa9a03cf345508583410
AdityaDighe <98305705+AdityaDighe@users.noreply.github.com>
2023-05-17 19:48:32 +0530

    Update nqueens.c

--- aoa sem4/backtracking/nqueens.c
+++ aoa sem4/backtracking/nqueens.c
@@ -1,80 +1,75 @@
-#define N 4
-#include <stdbool.h>
-#include <stdio.h>
-
-void printSolution(int board[N][N])
-{
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if(board[i][j])
-				printf("Q ");
-			else
-				printf(". ");
-		}
-		printf("\n");
-	}
-}
-
-bool isSafe(int board[N][N], int row, int col)
-{
-	int i, j;
-
-	for (i = 0; i < col; i++)
-		if (board[row][i])
-			return false;
-
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-		if (board[i][j])
-			return false;
-
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
-		if (board[i][j])
-			return false;
-
-	return true;
-}
-
-bool solveNQUtil(int board[N][N], int col)
-{
-	if (col >= N)
-		return true;
-
-	for (int i = 0; i < N; i++) {
-		
-
-		if (isSafe(board, i, col)) {
-			
-			board[i][col] = 1;
-
-			
-			if (solveNQUtil(board, col + 1))
-				return true;
-
-			board[i][col] = 0;
-		}
-	}
-
-	return false;
-}
-
-bool solveNQ()
-{
-	int board[N][N] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
-
-	if (solveNQUtil(board, 0) == false) {
-		printf("Solution does not exist");
-		return false;
-	}
-
-	printSolution(board);
-	return true;
-}
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+ 
+int board[20],count;
+ 
 int main()
 {
-	solveNQ();
-	return 0;
+ int n,i,j;
+ void queen(int row,int n);
+ 
+ printf("Enter number of Queens:");
+ scanf("%d",&n);
+ queen(1,n);
+ return 0;
+}
+ 
+//function for printing the solution
+void print(int n)
+{
+ int i,j;
+ printf("\n\nSolution %d:\n\n",++count);
+ 
+ for(i=1;i<=n;++i)
+ {
+  printf("\t%d",i);
+ }
+ 
+ for(i=1;i<=n;++i)
+ { 
+  printf("\n\n%d",i);
+  for(j=1;j<=n;++j) //for nxn board
+  {
+   if(board[i]==j)
+    printf("\tQ"); //queen at i,j position
+   else
+    printf("\t-"); //empty slot
+  }
+ }
+}
+ 
+/*funtion to check conflicts
+If no conflict for desired postion returns 1 otherwise returns 0*/
+int place(int row,int column)
+{
+ int i;
+ for(i=1;i<=row-1;++i)
+ {
+  //checking column and digonal conflicts
+  if(board[i]==column)
+   return 0;
+  else
+   if(abs(board[i]-column)==abs(i-row))
+    return 0;
+}
+ 
+return 1; //no conflicts
+}
+ 
+//function to check for proper positioning of queen
+void queen(int row,int n)
+{
+ int column;
+ for(column=1;column<=n;++column)
+ {
+  if(place(row,column))
+  {
+   board[row]=column; //no conflicts so place queen
+   if(row==n) //dead end
+    print(n); //printing the board configuration
+   else //try queen with next position
+    queen(row+1,n);
+  }
+ }
 }
